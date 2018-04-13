c634f1a5feb3615acde19a217311ef3d38fbcb70
Marcelle <yanarimy@gmail.com>
2018-04-13 01:12:54 -0300

    It worked! Nice buttons and styling.

--- src/components/BurgerContents/BuildControls/BuildControl/BuildControl.js
+++ src/components/BurgerContents/BuildControls/BuildControl/BuildControl.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import cssClasses from './BuildControl.css';
 import btnClasses from './Buttons.css';
+import classnames from 'classnames';
 
 const buildControl = (props) => {
     const btn = [
         btnClasses.button,
-        btnClasses.buttonAction,
-        btnClasses.buttonCircle,
-        btnClasses.buttonSmall
-    ].join();
+        btnClasses.buttonSmall,
+        btnClasses.buttonBox,
+        btnClasses.buttonSecondary,
+        btnClasses.buttonLongshadow
+    ];
 
     return(
         <div className={cssClasses.BuildControl}>
             <div className={cssClasses.Label}>{props.label}</div>
-            <button className={btn}><i class="fa fa-minus"></i></button>
-            <button className={btn}><i class="fa fa-plus"></i></button>
+            <button className={classnames(btn)}><i class="fa fa-minus"></i></button>
+            <button className={classnames(btn)}><i class="fa fa-plus"></i></button>
         </div>
     );
 };
