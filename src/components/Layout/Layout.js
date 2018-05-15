84b9a7c88b9a7aeec1747d1681c11a628c83d949
Marcelle <yanarimy@gmail.com>
2018-05-15 00:29:41 -0300

    useless footer removed, colors changed to triadic palette

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React, {Fragment} from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-import Footer from '../Footer/Footer';
 import cssCl from './Layout.css';
 
 // Aux HOC = wrapping something and immediately outputting it
@@ -12,7 +11,6 @@ const layout = (props) => (
         <main className={cssCl.MainContent}>
             {props.children}
         </main>
-        <Footer />
     </Fragment>
 );
 
