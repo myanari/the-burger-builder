222d3c106647dcdd9b849557218a226cb48a05fd
Marcelle <yanarimy@gmail.com>
2018-05-15 22:11:15 -0300

    animation to close sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,24 @@
 import React, { Fragment } from 'react';
-import cssCl from './SideDrawer.css';
+import classNames from 'classnames/bind';
+import styles from './SideDrawer.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Logo from '../../Logo/Logo';
 
 const sideDrawer = (props) => {
-	const attachedClasses = [cssCl.SideDrawer, ]
+	let cx = classNames.bind(styles);
+	let attachedClasses = cx({
+		SideDrawer: true,
+		Open: props.open,
+		Close: !props.open
+	});
+	console.log(attachedClasses);
   return (
   	<Fragment>
 			<Backdrop show={props.open} leaveBackdrop={props.closed} />
-			<div className={cssCl.SideDrawer} style={{marginBottom: '16px'}}>
+			<div className={attachedClasses} style={{marginBottom: '16px'}}>
 				<Logo height="11%" />
-				<nav className={cssCl.DesktopOnly}>
+				<nav className={styles.DesktopOnly}>
 					<NavigationItems />
 				</nav>
 			</div>
