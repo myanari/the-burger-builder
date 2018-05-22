e2020961550dc68d563c93df3512e1efeec695c9
Marcelle <yanarimy@gmail.com>
2018-05-22 11:15:20 -0300

    button adapted to receive strings, btn cmp turned to class cmp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 	render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Fragment>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </Fragment>
     );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,23 +2,35 @@ import React from 'react';
 import btnClasses from './Button.css';
 import classnames from 'classnames'
 
-const standardClassBtn = [
-    btnClasses.button,
-    btnClasses.buttonBox,
-    btnClasses.button3d,
-    btnClasses.buttonLongShadow
-];
-
 // Instead of Success and Danger like Mr Schwartzmüller's project,
 // The classes instead are gonna be Action and Caution
 
 // My project's: buttonAction = Max's project: Success
 // My project's: buttonCaution = Max's project: Caution
-const button = (props) => (
-    <button
-        style={{margin: '3px'}}
-        className={classnames(standardClassBtn, btnClasses[props.btnType])} 
-        onClick={props.clicked}>{props.children}</button>
-);
+class button extends React.Component {
+    state = {
+        stdStyles: [btnClasses.button,
+            btnClasses.buttonLongShadow,
+            btnClasses.button3d,
+            btnClasses.buttonBox]
+    }
+    componentDidMount() {
+        if (typeof this.props.children === 'string') {
+            let styles = [btnClasses.button, btnClasses.buttonRounded, btnClasses.buttonGlow];
+            this.setState({stdStyles: styles}, () => console.log(this.state));
+
+        }
+    }
+    render() {
+        const classes = classnames(this.state.stdStyles, btnClasses[this.props.btnType]);
+        return(
+            <button
+                style={{margin: '3px'}}
+                className={classes} 
+                onClick={this.props.clicked}>{this.props.children}</button>
+        );
+    }
+    
+};
 
 export default button;
