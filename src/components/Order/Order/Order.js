0b9547eae2e3fcc55ec2a8fd15496549c25e25be
Marcelle <yanarimy@gmail.com>
2018-05-22 19:13:20 -0300

    styled span

--- src/components/Order/Order/Order.js
+++ src/components/Order/Order/Order.js
@@ -12,13 +12,25 @@ const order = (props) => {
   }
 
   const output = ingredients.map(ig => {
-    return <span key={ig.name}>{ig.name} ({ig.amount})</span>
+    return (
+      <span
+        key={ig.name}
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }}>
+        {ig.name} ({ig.amount})
+      </span>
+    );
   });
 
   return(
     <div className={styles.Order}>
-      <p>Ingredients: US$ {output}</p>
-      <p>Price <strong>{Number(props.data.price).toFixed(2)}</strong></p>
+      <p>Ingredients: {output}</p>
+      <p>Price: <strong>US$ {Number(props.data.price).toFixed(2)}</strong></p>
     </div>
   );
 };
