240a5446f97d14ff51e558cb37d6eb28bda88e7c
Marcelle <yanarimy@gmail.com>
2018-05-22 16:50:48 -0300

    setting up router to orders and order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Orders from './containers/Orders/Orders';
 import Checkout from './containers/Checkout/Checkout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Layout from './components/Layout/Layout';
@@ -11,6 +12,7 @@ class App extends Component {
       <Layout>
       <Switch>
         <Route path='/' exact component={BurgerBuilder} />
+        <Route path='/orders' component={Orders} />
         <Route path='/checkout' component={Checkout} />
       </Switch>
       </Layout>
