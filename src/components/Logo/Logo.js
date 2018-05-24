ef89f30e30665572178be2fc1137e67aa0840fb7
Marcelle <yanarimy@gmail.com>
2018-05-24 00:32:40 -0300

    setting up a link when logo is clicked

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './Logo.css';
 import burgerLogo from '../../assets/images/logo.png';
 
 const logo = (props) => (
 	<div className={styles.Logo} style={{height: props.height}}>
+		<Link to="/">
 		<img src={burgerLogo} alt="My Burger Logo"/>
+		</Link>
 	</div>
 );
 
