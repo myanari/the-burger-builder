2862f865b9788f517fb7313db4fc66e277824627
Marcelle <yanarimy@gmail.com>
2018-05-27 22:27:11 -0300

    setting up the price and disabled state in order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,17 +10,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
-const INGREDIENT_PRICES = {
-  salad: 0.5,
-  cheese: 0.4,
-  meat: 1.3,
-  bacon: 0.8
-}
-
 class BurgerBuilder extends Component {
   state = {
-    totalPrice: 5,
-    purchaseable: false,
     purchasing: false,
     loading: false
   };
@@ -37,41 +28,8 @@ class BurgerBuilder extends Component {
       .map(ingKey =>  ingredients[ingKey] )
       .reduce((sum, el) => sum + el, 0);
 
-    this.setState({ purchaseable: sum > 0 });
+    return sum > 0;
   }
-
-  addIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type];
-    const updatedCount = oldCount + 1;
-    const updatedIngredients = { ...this.state.ingredients };
-
-    updatedIngredients[type] = updatedCount;
-
-    const priceAddition = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceAddition;
-
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchaseState(updatedIngredients);
-  };
-
-  removeIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type];
-
-    if (oldCount <= 0) { return }
-
-    const updatedCount = oldCount - 1;
-    const updatedIngredients = { ...this.state.ingredients };
-
-    updatedIngredients[type] = updatedCount;
-
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
-
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchaseState(updatedIngredients);
-  };
 
   purchaseHandler = () => {
     this.setState({ purchasing: true });
@@ -114,8 +72,8 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded} 
             ingredientRemoved={this.props.onIngredientRemoved} 
             disabled={disabledInfo} 
-            price={this.state.totalPrice} 
-            purchaseable={this.state.purchaseable} />
+            price={this.props.price} 
+            purchaseable={this.updatePurchaseState(this.props.ings)} />
         </Fragment>
       );
       orderSummary = (
@@ -123,7 +81,7 @@ class BurgerBuilder extends Component {
           canceled={this.cancelPurchaseHandler}
           continue={this.continuePurchaseHandler}
           ingredients={this.props.ings}
-          price={this.state.totalPrice} />
+          price={this.props.price} />
       );
       if (this.state.loading) {
         orderSummary = <Spinner />
@@ -144,7 +102,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients
+    ings: state.ingredients,
+    price: state.totalPrice
   };
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,11 +9,20 @@ const initialState = {
   },
   totalPrice: 4
 }
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.8
+}
+
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.ADD_INGREDIENT:
       return {
         ...state,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
@@ -22,6 +31,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.REMOVE_INGREDIENT:
       return {
         ...state,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
