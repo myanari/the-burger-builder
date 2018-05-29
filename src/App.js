d8ccb2b563582c83a8e7c50fb2a6090cccb7f7b0
Marcelle <yanarimy@gmail.com>
2018-05-29 03:32:05 -0300

    setting up auth, validators to auth, navigation items to auth path, route to auth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Orders from './containers/Orders/Orders';
 import Checkout from './containers/Checkout/Checkout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Layout from './components/Layout/Layout';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
 	render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Route path='/' exact component={BurgerBuilder} />
         <Route path='/orders' component={Orders} />
         <Route path='/checkout' component={Checkout} />
+        <Route path='/auth' component={Auth} />
       </Switch>
       </Layout>
     );
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import validator from 'validator';
+
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import styles from './Auth.css';
+
+class Auth extends Component {
+	state = {
+		controls: {
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Mail Address'
+				},
+				value: '',
+				valid: false,
+				touched: false,
+				validation: {
+					required: true,
+					email: true
+				}
+			},
+			password: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'password',
+					placeholder: 'Your password'
+				},
+				value: '',
+				valid: false,
+				touched: false,
+				validation: {
+					required: true,
+					password: true
+				}
+			}
+		}
+	};
+
+	static checkValidity(value, rules) {
+		let isValid = true;
+		if (rules) {
+			if (rules.required) {
+				isValid = validator.trim(value) !== '' && isValid;
+			}
+			if (rules.email) {
+				isValid = validator.isEmail(validator.trim(value)) && isValid;
+			}
+			if (rules.password) {
+				isValid = validator.isLength(validator.trim(value), {min: 6}) && isValid;
+			}
+		}
+		return isValid;
+	}
+
+	inputChangedHandler = (event, controlName) => {
+		const updatedControls = {
+			...this.state.controls,
+			[controlName]: {
+				...this.state.controls[controlName],
+				value: event.target.value,
+				valid: Auth.checkValidity(event.target.value, this.state.controls[controlName].validation),
+				touched: true
+			}
+		};
+		this.setState({controls: updatedControls});
+	};
+
+	render() {
+		const formElements = [];
+		for (let key in this.state.controls) {
+			formElements.push({
+				id: key,
+				config: this.state.controls[key]
+			});
+		}
+
+		const form = formElements.map(el => (
+			<Input
+				key={el.id}
+				elementType={el.config.elementType}
+				elementConfig={el.config.elementConfig}
+				value={el.config.value}
+				invalid={!el.config.valid && el.config.touched}
+				shouldValidate={el.config.validation}
+				changed={(event) => this.inputChangedHandler(event, el.id)} />
+		));
+		return (
+			<div className={styles.Auth}>
+				<form>
+					{form}
+					<Button btnType="buttonAction">Submit</Button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Auth;
