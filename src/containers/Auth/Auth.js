ae220c5849f1e9553d8cf0460e71ba92c9713a08
Marcelle <yanarimy@gmail.com>
2018-05-29 19:46:52 -0300

    auth to sign in implemented

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import validator from 'validator';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import styles from './Auth.css';
+import * as actions from '../../store/actions';
 
 class Auth extends Component {
 	state = {
@@ -68,6 +70,11 @@ class Auth extends Component {
 		this.setState({controls: updatedControls});
 	};
 
+	onSubmitHandler = (event) => {
+		event.preventDefault();
+		this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+	};
+
 	render() {
 		const formElements = [];
 		for (let key in this.state.controls) {
@@ -89,7 +96,7 @@ class Auth extends Component {
 		));
 		return (
 			<div className={styles.Auth}>
-				<form>
+				<form onSubmit={this.onSubmitHandler}>
 					{form}
 					<Button btnType="buttonAction">Submit</Button>
 				</form>
@@ -98,4 +105,10 @@ class Auth extends Component {
 	}
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+	return {
+		onAuth: (email, password) => dispatch(actions.auth(email, password))
+	};
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
