cb236c06858327c7dfb805fb459d207f1ee52f36
Marcelle <yanarimy@gmail.com>
2018-06-13 01:29:07 -0300

    moving more logic into saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,10 +1,18 @@
+import { delay } from 'redux-saga';
 import { put } from 'redux-saga/effects';
 
 import * as actionTypes from "../actions/actionTypes";
+import * as actions from '../actions';
+import {logout} from "../actions/auth";
 
 export function* logoutSaga() {
 	yield localStorage.removeItem('token');
 	yield localStorage.removeItem('userId');
 	yield localStorage.removeItem('expirationTime');
-	yield put({ type: actionTypes.AUTH_LOGOUT });
+	yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+	yield delay(action.expirationTime * 1000);
+	yield put(actions.logout());
 }
